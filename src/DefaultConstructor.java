a2900185b7d4dcf17c5046530e9eed7a88ad8334
SRINIVAS MAMIDISETTI <SRINIVAS MAMIDISETTI@DESKTOP-0M4NJGP.domain.name>
2023-12-10 11:04:00 +0530

    Constructor

---
+++ src/DefaultConstructor.java
@@ -0,0 +1,18 @@
+
+public class DefaultConstructor {//class starts
+	
+	
+	DefaultConstructor()//create on constructor
+	{
+		System.out.println("this is default constructor");//print statement
+		System.out.println("also,no argument constructor");//print statement
+	}
+
+	public static void main(String[] args) {//main method of the class .its a prototype method
+		
+		new DefaultConstructor();//create an object using new keyword.without any reference
+		
+
+	}
+
+}//class ends
